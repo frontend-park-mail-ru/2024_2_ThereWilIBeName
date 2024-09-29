ffadfefb40ca521993514829510c76ab401f2690
Григорий Громыко <multiplaygames@mail.ru>
2024-09-30 02:58:52 +0300

    fix background css

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -3,11 +3,74 @@ import RESTApi from './RESTApi.js';
 export default {
 
     async getAttractions() {
-        const res = await RESTApi.get('/api/v1/places');
+        // const res = await RESTApi.get('/api/v1/places');
+        // return {
+        //     data: res.data,
+        //     status: res.status,
+        //     ok: res.ok,
+        // };
+
         return {
-            data: res.data,
-            status: res.status,
-            ok: res.ok,
+            data:
+                [
+                    {
+                        'id': 1,
+                        'image': 'https://cdn.tripster.ru/thumbs2/725b6ada-9815-11ed-87d9-ee8b4c069e8d.1220x600.jpeg',
+                        'name': 'Московский Кремль',
+                        'description': 'Московский Кремль'
+                    },
+                    {
+                        'id': 1,
+                        'image': 'https://cdn.tripster.ru/thumbs2/725b6ada-9815-11ed-87d9-ee8b4c069e8d.1220x600.jpeg',
+                        'name': 'Московский Кремль',
+                        'description': 'Московский Кремль'
+                    },
+                    {
+                        'id': 1,
+                        'image': 'https://cdn.tripster.ru/thumbs2/725b6ada-9815-11ed-87d9-ee8b4c069e8d.1220x600.jpeg',
+                        'name': 'Московский Кремль',
+                        'description': 'Московский Кремль'
+                    },
+                    {
+                        'id': 1,
+                        'image': 'https://cdn.tripster.ru/thumbs2/725b6ada-9815-11ed-87d9-ee8b4c069e8d.1220x600.jpeg',
+                        'name': 'Московский Кремль',
+                        'description': 'Московский Кремль'
+                    },
+                    {
+                        'id': 1,
+                        'image': 'https://cdn.tripster.ru/thumbs2/725b6ada-9815-11ed-87d9-ee8b4c069e8d.1220x600.jpeg',
+                        'name': 'Московский Кремль',
+                        'description': 'Московский Кремль'
+                    },
+                    {
+                        'id': 1,
+                        'image': 'https://cdn.tripster.ru/thumbs2/725b6ada-9815-11ed-87d9-ee8b4c069e8d.1220x600.jpeg',
+                        'name': 'Московский Кремль',
+                        'description': 'Московский Кремль'
+                    },
+                    {
+                        'id': 1,
+                        'image': 'https://cdn.tripster.ru/thumbs2/725b6ada-9815-11ed-87d9-ee8b4c069e8d.1220x600.jpeg',
+                        'name': 'Московский Кремль',
+                        'description': 'Московский Кремль'
+                    },
+                    {
+                        'id': 1,
+                        'image': 'https://cdn.tripster.ru/thumbs2/725b6ada-9815-11ed-87d9-ee8b4c069e8d.1220x600.jpeg',
+                        'name': 'Московский Кремль',
+                        'description': 'Московский Кремль'
+                    },
+                    {
+                        'id': 1,
+                        'image': 'https://cdn.tripster.ru/thumbs2/725b6ada-9815-11ed-87d9-ee8b4c069e8d.1220x600.jpeg',
+                        'name': 'Московский Кремль',
+                        'description': 'Московский Кремль'
+                    }
+
+                ],
+            status: '200',
+            ok: 'ok'
         };
     },
 
