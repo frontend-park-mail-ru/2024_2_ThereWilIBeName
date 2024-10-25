a7d564f0898a7869fa515fbed50e9b7fac447712
Григорий Громыко <multiplaygames@mail.ru>
2024-10-25 19:45:40 +0300

    profile page add

--- src/pages/profile.ts
+++ src/pages/profile.ts
@@ -2,7 +2,15 @@ import Router from '../utils/Router'
 
 export default {
 
-    html: ``,
+    html: `<header class="header">
+            <div class="logo" >
+                <img src="/src/static/logo.png" alt="Логотип" class="logo-image">
+            </div>
+        </header>
+        <main class="warn-message">
+            <div class="warning">Здесь будет профиль :)</div>
+            <button class="unknown-page-button" id="error-button">На главную</button>
+        </main>`,
 
     async mount(router: Router): Promise<void> {
 
