814ecf6900b0863d7a8ce16b401aab42fa18048e
Григорий Громыко <multiplaygames@mail.ru>
2024-11-07 13:17:59 +0300

    fix profile gallery text 6

--- src/pages/profilePage/profileMenu.ts
+++ src/pages/profilePage/profileMenu.ts
@@ -13,6 +13,7 @@ export default async function updateMenu(activeMenuButton: HTMLElement) {
         const tripsResponse = await Api.getUserTrips(User.id);
         const trips = tripsResponse.data;
         const galleryProfileElement = document.getElementById('gallery-profile') as HTMLElement;
+        galleryProfileElement.innerHTML = '';
         galleryProfileElement.textContent = 'Здесь будут ваши поездки';
         if (tripsResponse.status === 200) {
             galleryProfileElement.innerHTML = galleryTemplateTrips({ trips, tripIcon });
@@ -30,6 +31,7 @@ export default async function updateMenu(activeMenuButton: HTMLElement) {
         };
         const achievements = achievementsResponse.data;
         const galleryProfileElement = document.getElementById('gallery-profile') as HTMLElement;
+        galleryProfileElement.innerHTML = '';
         galleryProfileElement.textContent = 'Здесь будут ваши достижения';
         if (achievementsResponse.ok) {
             galleryProfileElement.innerHTML = galleryTemplateAchievements({ achievements, defaultAchievementIcon });
@@ -45,6 +47,7 @@ export default async function updateMenu(activeMenuButton: HTMLElement) {
         };
         const reviews = reviewsResponse.data;
         const galleryProfileElement = document.getElementById('gallery-profile') as HTMLElement;
+        galleryProfileElement.innerHTML = '';
         galleryProfileElement.textContent = 'Здесь будут ваши отзывы';
         if (reviewsResponse.status === 200) {
             galleryProfileElement.innerHTML = galleryTemplateReviews({ reviews });
