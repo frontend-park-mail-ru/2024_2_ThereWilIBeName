ed6b60f61cdfaace1f2870ecd4ac24834d8aa9a3
Григорий Громыко <multiplaygames@mail.ru>
2024-11-08 23:58:50 +0300

    in progress palms

--- src/pages/profilePage/profileMenu.ts
+++ src/pages/profilePage/profileMenu.ts
@@ -3,6 +3,7 @@ import galleryTemplateAchievements from './profileAchievements.hbs';
 import galleryTemplateReviews from './profileReviews.hbs';
 import myBlackIcon from '../../static/232323.png';
 import inProgressPng from '../../static/in progress.png';
+import palmsImg from '../../static/please black.png';
 import Api from '../../utils/Api';
 import User from '../../utils/user';
 
@@ -14,34 +15,46 @@ export default async function updateMenu(activeMenuButton: HTMLElement) {
     if (activeMenuButton.textContent === 'Поездки') {
         const tripsResponse = await Api.getUserTrips(User.id);
         const trips = tripsResponse.data;
+        const galleryProfileElement = document.getElementById('gallery-profile') as HTMLElement;
         if (trips) {
-            const galleryProfileElement = document.getElementById('gallery-profile') as HTMLElement;
             galleryProfileElement.innerHTML = '';
             galleryProfileElement.innerHTML = galleryTemplateTrips({ trips, tripIcon, myBlackIcon });
         }
+        else {
+            galleryProfileElement.innerHTML =
+                `<div class="please-block">
+                    <img src="${palmsImg}" class="please-img">
+                    <div class="auth-please" id="auth-please">У вас нет поездок</div>
+                </div>`;
+        }
+
     }
 
     if (activeMenuButton.textContent === 'Достижения') {
         // const achievementsResponse = await Api.getAchievements();
         // const achievements = achievementsResponse.data;
-        const achievements = [
-            {
-                placeName: 'Москва',
-                rating: '5',
-                reviewText: 'Очень понравилось место. Остался бы здесь жить'
-            },
-        ];
         const galleryProfileElement = document.getElementById('gallery-profile') as HTMLElement;
-        galleryProfileElement.innerHTML = galleryTemplateAchievements({ achievements, defaultAchievementIcon });
-        galleryProfileElement.insertAdjacentHTML('beforeend', `<img src="${ inProgressPng }" class="in-progress-img">`);
+        galleryProfileElement.innerHTML =
+            `<div class="please-block">
+                <img src="${palmsImg}" class="please-img">
+                <div class="auth-please" id="auth-please">Достижения в разработке</div>
+            </div>`;
+        // galleryProfileElement.innerHTML = galleryTemplateAchievements({ achievements, defaultAchievementIcon });
     }
 
     if (activeMenuButton.textContent === 'Отзывы') {
-        // const reviewsResponse = await Api.getUserReviews(User.id);
-        // const reviews = reviewsResponse.data;
-        const reviews = [{},{},{},{}];
+        const reviewsResponse = await Api.getUserReviews(User.id);
+        const reviews = reviewsResponse.data;
         const galleryProfileElement = document.getElementById('gallery-profile') as HTMLElement;
-        galleryProfileElement.innerHTML = galleryTemplateReviews({ reviews });
-        galleryProfileElement.insertAdjacentHTML('beforeend', `<img src="${ inProgressPng }" class="in-progress-img">`);
+        if (reviews) {
+            galleryProfileElement.innerHTML = galleryTemplateReviews({ reviews });
+        }
+        else {
+            galleryProfileElement.innerHTML =
+                `<div class="please-block">
+                <img src="${palmsImg}" class="please-img">
+                <div class="auth-please" id="auth-please">Список отзывов в разработке</div>
+            </div>`;
+        }
     }
 };
