d533575a661bbc6cee0b3681192c617d6dc50c55
Григорий Громыко <multiplaygames@mail.ru>
2024-11-14 18:57:31 +0300

    fix columns add 3

--- src/pages/homePage/home.ts
+++ src/pages/homePage/home.ts
@@ -37,8 +37,9 @@ export default {
         await header.mount(router);
 
         // Загрузка достопримечательностей
-        const attractionsResponse = await Api.getAttractions();
-        const attractions = attractionsResponse.data;
+        // const attractionsResponse = await Api.getAttractions();
+        // const attractions = attractionsResponse.data;
+        const attractions = [{},{},{},{},{},{},{},{},{},{},];
         const galleryElement = document.getElementById('gallery') as HTMLElement;
         galleryElement.innerHTML = galleryTemplate({ attractions });
 
@@ -46,7 +47,7 @@ export default {
         placeButton.addEventListener('click', (event) => {
             const target = event.target as HTMLElement;
             if (target) {
-                const listItem = target.closest('li');
+                const listItem = target.closest('div');
                 if (listItem) {
                     const itemId = listItem.querySelector('a')!.href.split('/').pop();
                     router.goto(`/places/${itemId}`);
