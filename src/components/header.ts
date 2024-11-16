192a5bd71032135f5a8d0c1b0e640db913bd0c05
Григорий Громыко <multiplaygames@mail.ru>
2024-11-16 15:12:34 +0300

    add home avatar

--- src/components/header.ts
+++ src/components/header.ts
@@ -2,16 +2,22 @@ import Router from '../utils/Router';
 import Api from '../utils/Api';
 import User from '../utils/user';
 import logoImage from '../static/logo trip black.svg';
+import searchButton from '../static/search button.svg';
 
 export default {
     html: `<header class="header">
                 <div class="logo">
                     <img src="${logoImage}" alt="Логотип" class="logo-image-black" id="logo-image">
                 </div>
-                <input type="text" placeholder="Здесь будет поиск" class="search">
+                <div class="search">
+                    <input type="text" placeholder="Здесь будет поиск" class="search-input">
+                    <img src="${searchButton}" alt="Поиск" class="search-button" id="search-button">
+                </div>
                 <button class="header-button" id="trips-button">Поездки</button>
                 <button class="header-button" id="signin-button">Вход</button>
-                <button class="user-button" id="user-button"></button>
+                <div class="user-button" id="user-button">
+                    <img class="avatar" alt="Аватарка">
+                </div>
             
                 <div id="side-menu" class="side-menu">
                     <div class="background-menu" id="background-menu"></div>
@@ -28,7 +34,7 @@ export default {
     async mount(router: Router): Promise<void> {
         const homeLogo = document.getElementById('logo-image') as HTMLElement;
         const signinButton = document.getElementById('signin-button') as HTMLButtonElement;
-        const userButton = document.getElementById('user-button') as HTMLButtonElement;
+        const userButton = document.getElementById('user-button') as HTMLImageElement;
         const sideMenu = document.getElementById('side-menu') as HTMLElement;
         const userNameDiv = document.getElementById('user-name') as HTMLElement;
         const backgroundMenu = document.getElementById('background-menu') as HTMLElement;
@@ -95,7 +101,10 @@ export default {
         User.email = currentUser.data.email;
         User.isSignedIn = true;
         signinButton.textContent = 'Сменить пользователя';
-        userButton.textContent = User.username;
+        const avatarPath = (await Api.getProfile(User.id)).data.avatarPath
+        if (avatarPath) {
+            userButton.src = avatarPath;
+        }
         userNameDiv.textContent = User.username;
         userButton.classList.add('show');
         signinButton.classList.add('hidden');
