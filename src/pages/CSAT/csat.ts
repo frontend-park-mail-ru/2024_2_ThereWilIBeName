41d070bf65e208fe090c9ef23a66f4542f71f668
Григорий Громыко <multiplaygames@mail.ru>
2024-11-23 14:34:23 +0300

    CSAT 32: fix iframe

--- src/pages/CSAT/csat.ts
+++ src/pages/CSAT/csat.ts
@@ -3,7 +3,17 @@ import csatHomeQuestion from './csat-home-question';
 
 export default {
     html: `
-        <div class="csat-root" id="csat-root"></div>
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+                <link href="/src/styles/csat.styl" rel="stylesheet" id="css-file">
+            </head>
+            <body>
+                <div class="csat-root" id="csat-root"></div>
+            </body>
+        </html>
     `,
 
     async mount(): Promise<void> {
