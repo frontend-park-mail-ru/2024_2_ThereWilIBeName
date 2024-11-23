1c35e178dbcebc3972b3f95030a3161cc79a32cd
Григорий Громыко <multiplaygames@mail.ru>
2024-11-23 13:40:55 +0300

    CSAT 26: fix csat-home-question

--- src/components/CSAT/csat-home-question.ts
+++ src/components/CSAT/csat-home-question.ts
@@ -2,8 +2,8 @@ export default {
     html: `
     <div class="home-question">
         <div class="csat-question-title">Как вам наш дизайн?</div>
-        <input type="text">
-        <button>Далее</button>
+        <input class="csat-question-input" type="text">
+        <button class="csat-question-button">Далее</button>
     </div>
     `
 };
