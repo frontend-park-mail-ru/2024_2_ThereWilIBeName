8f8eb7c4a19b339787c71005d6e9f04c32c24739
Григорий Громыко <multiplaygames@mail.ru>
2024-11-23 13:57:34 +0300

    CSAT 28: fix csat-home-question

--- src/components/CSAT/csat-home-question.ts
+++ src/components/CSAT/csat-home-question.ts
@@ -2,7 +2,7 @@ export default {
     html: `
     <div class="home-question">
         <div class="csat-question-title">Как вам наш дизайн?</div>
-        <input class="csat-question-input" type="text">
+        <textarea class="csat-question-input"></textarea>
         <button class="csat-question-button">Далее</button>
     </div>
     `
