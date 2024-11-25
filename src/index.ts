f298aa261efa36d93d296144bd0c4790b6635814
Григорий Громыко <multiplaygames@mail.ru>
2024-11-25 12:15:53 +0300

    turn on service worker

--- src/index.ts
+++ src/index.ts
@@ -20,31 +20,31 @@ const router: Router = new Router(routes, 'root');
 //     csat.mount;
 // });
 
-//
-// if ('serviceWorker' in navigator) {
-//     (async () => {
-//         try {
-//             const reg = await navigator.serviceWorker.register('/service-worker.js', { scope: '/' });
-//             console.log('Регистрация прошла успешно. Область видимости запросов: ' + reg.scope);
-//             const data = {
-//                 type: 'CACHE_URLS',
-//                 payload: [
-//                     location.href,
-//                     ...performance.getEntriesByType('resource').map(({ name }) => name)
-//                 ]
-//             };
-//             const { installing, waiting, active } = reg;
-//             if (installing) {
-//                 console.log('Service worker installing');
-//             } else if (waiting) {
-//                 console.log('Service worker installed');
-//             } else if (active) {
-//                 active.postMessage(data);
-//                 console.log('Service worker active');
-//             }
-//         }
-//         catch(error) {
-//             console.log('Регистрация не произведена. ' + error);
-//         }
-//     })();
-// }
+
+if ('serviceWorker' in navigator) {
+    (async () => {
+        try {
+            const reg = await navigator.serviceWorker.register('/service-worker.js', { scope: '/' });
+            console.log('Регистрация прошла успешно. Область видимости запросов: ' + reg.scope);
+            const data = {
+                type: 'CACHE_URLS',
+                payload: [
+                    location.href,
+                    ...performance.getEntriesByType('resource').map(({ name }) => name)
+                ]
+            };
+            const { installing, waiting, active } = reg;
+            if (installing) {
+                console.log('Service worker installing');
+            } else if (waiting) {
+                console.log('Service worker installed');
+            } else if (active) {
+                active.postMessage(data);
+                console.log('Service worker active');
+            }
+        }
+        catch(error) {
+            console.log('Регистрация не произведена. ' + error);
+        }
+    })();
+}
