d8e4e7070ae6cc352fa68a49000894ef856f6458
Григорий Громыко <multiplaygames@mail.ru>
2024-12-18 15:30:08 +0300

    fix new trip page 3.5

--- src/pages/tripPage/mountPhotos.ts
+++ src/pages/tripPage/mountPhotos.ts
@@ -1,8 +1,8 @@
 import Api from '../../utils/Api';
 import galleryPhotosTemplate from './trip-photos.hbs';
 
-export default async function mountPhotos(galleryItem: Element, photos: any | null = null) {
-    if (photos !== null) {
+export default async function mountPhotos(galleryItem: Element, photos: { photoPath: string }[]) {
+    if (photos.length !== 0) {
         galleryItem.innerHTML = galleryPhotosTemplate({photos});
     }
 };
